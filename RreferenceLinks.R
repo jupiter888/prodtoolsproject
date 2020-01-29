5fa4125e20272fa325e5aebe8ac35c20df464a2e
Daniel <visual.eyes.success@gmail.com>
2020-01-29 07:02:37 +0100

    Add files via upload
    R markdown links

---
+++ RreferenceLinks.R
@@ -0,0 +1,7 @@
+#Links for R markdown
+
+
+#step by step introduction 
+# https://rmarkdown.rstudio.com
+#Quick reference cheatsheet 
+# https://rstudio.com/wp-content/uploads/2015/02/rmarkdown-cheatsheet.pdf
